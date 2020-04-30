be150d55b4db3f4b92dde494beef04cc88b40e9a
aldum63 <53421370+aldum63@users.noreply.github.com>
2020-04-30 16:51:04 +0200

    Add files via upload

--- php/Annotations.php
+++ php/Annotations.php
@@ -11,6 +11,9 @@ use xml\Data;
 
 class Annotations extends \xml\Helper {
     protected $emoji = [];
+    protected const DEFAULT_DATA_DIR = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'emoji';
+    public const CSV_HEADER = "\xEF\xBB\xBF"; // this is UTF-8 header to force Excel to recognize national characters in CSV file
+    public const CSV_VALUE_SEPARATOR = ','; // ; is recommended for Excel
 
     public function parse(string $langs, array $emoji = []) {
         $langs = $this->getLanguageList($langs, ['annotationsDerived', 'annotations']);
@@ -99,4 +102,97 @@ class Annotations extends \xml\Helper {
             echo PHP_EOL;
         }
     }
+
+    /**
+     * Reads CSV files and converts it into 2-dimensional array.
+     *
+     * @param  string $filename Path and name of a CSV file. To read input use 'php://stdin' (CLI) or 'php://input' (POST). Other protocol wrappers may be supported as well.
+     * @param  bool|array[string] $keys (optional, default: false)
+     *                                  False = Just convert the file to array without any additional processing.
+     *                                  True = Consider first line of the file as column names and use them as array keys.
+     *                                  When array given it will be used as array keys for the rows (including the first one).
+     *                                  Rows with more columns than $keys will be trimmed, rows with less columns will be skipped!!!
+     * @param  bool $strictIndex (optional, default: false)
+     *                                  False = output array (in 1st dimension) will have numeric keys from 0 to maxRows.
+     *                                  True  = output array (in 1st dimension) will have keys starting with 1
+     *                                          and indexes of empty or otherwise skipped rows will be missing in the array.
+     *                                  Example: [1 => [...], 2 => [...], 5 => [...]]; //rows 3 and 4 were empty
+     *                                  When both $keys and $stringIndex are True the output array will start with index 2
+     *                                   (because the first row with column names will be skipped).
+     *                                  Strict index follows row numbering used for files in IDEs and is intended for debugging.
+     * @return array[array[string]] Parsed file. Empty rows are not included. First row is not included when $keys == true.
+     */
+    public static function parseCsv(string $filename, $keys = false, $strictIndex = false) : array {
+        $filename = self::DEFAULT_DATA_DIR . DIRECTORY_SEPARATOR . $filename;
+        if (!is_readable($filename)) { //When file not exists...
+            return [];
+        }
+
+        $f = fopen($filename, 'r');
+        $output = [];
+        $rowIndex = 0;
+        $firstRow = true; //to detect when we read the very first row in the file
+
+        if (false === $f) { //When failed to load the file...
+            return [];
+        }
+
+        try {
+            ini_set('auto_detect_line_endings', true); //allow to parse other OS's line endings
+            if (true === $keys) {
+                $keys = fgetcsv($f, 0, self::CSV_VALUE_SEPARATOR);
+                ++$rowIndex;
+                if (count($keys)) { //remove BOM from keys
+                    if (strncmp($keys[0], self::CSV_HEADER, strlen(self::CSV_HEADER)) === 0) {
+                        $pos = strpos($keys[0], self::CSV_HEADER);
+                        $keys[0] = $pos === false ? false : (string) substr($keys[0], $pos + strlen(self::CSV_HEADER));
+                    }
+                    $firstRow = false;
+                }
+            }
+            while ($row = fgetcsv($f, 0, self::CSV_VALUE_SEPARATOR)) {
+                ++$rowIndex;
+                if (empty($row)) {
+                    continue; //ignore empty rows (returned as empty array)
+                }
+                if ($firstRow) { //remove BOM from first row
+                    if (strncmp($row[0], self::CSV_HEADER, strlen(self::CSV_HEADER)) === 0) {
+                        $pos = strpos($row[0], self::CSV_HEADER);
+                        $row[0] = $pos === false ? false : (string) substr($row[0], $pos + strlen(self::CSV_HEADER));
+                    }
+                    $firstRow = false;
+                }
+
+                if (is_array($keys)) { //append keys for each row
+                    $row = array_combine($keys, array_slice($row, 0, count($keys)));
+                    if (false === $row) {
+                        continue; //row does not have enough cells required by $keys
+                    }
+                }
+
+                if ($strictIndex) {
+                    $output[$rowIndex] = $row;
+                }
+                else {
+  //                  $output[] = $row;
+                    foreach($row as $lang=>$value){
+                        if ($lang==='key'){
+                            $thiskey = $row[$lang];
+                        } else{
+                            $output[$lang][$thiskey] = $value;
+                        }
+
+                    }
+
+                }
+            }
+        }
+        finally {
+            fclose($f);
+        }
+
+        return $output;
+    }
+
+
 }
